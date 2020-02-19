fe4becd9c75f85d27efe4fea9d47e1fa7962838c
alexisOller <alexisolleraw@gmail.com>
2020-02-19 23:48:26 +0100

    antes de ejercicios
    19-02-2020

---
+++ Assets/Scripts/Move_Sphere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Sphere : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public float forceValue ;
+    public float jumpValue;
+    private Rigidbody rb_sphere;
+    private AudioSource auSour_sphere;
+
+    void Start() {
+        rb_sphere = GetComponent<Rigidbody>();
+        auSour_sphere=GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb_sphere.velocity.y)<0.01f) {
+            rb_sphere.AddForce(Vector3.up * jumpValue,ForceMode.Impulse);
+            auSour_sphere.Play();
+
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb_sphere.AddForce(new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))*forceValue);
+
+
+    }
+}
+
